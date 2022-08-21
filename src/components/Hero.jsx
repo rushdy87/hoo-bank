bc689a3f6b18f6578108c7d7b9e1ea9920f7fc96
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2022-08-21 05:05:48 +0300

    Hero Component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,53 @@
-const Hero = () => {
-  return <div>Hero</div>;
-};
+import GetStarted from './GetStarted';
+import styles from '../style';
+import { discount, robot } from '../assets';
+
+const Hero = () => (
+  <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+    <div
+      className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
+    >
+      <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+        <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
+        <p className={`${styles.paragraph} ml-2`}>
+          <span className="text-white">20%</span> Discount For{' '}
+          <span className="text-white">1 Month</span> Account
+        </p>
+      </div>
+
+      <div className="flex flex-row justify-between items-center w-full">
+        <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
+          The Next <br /> {''} <span className="text-gradient">Genration</span>{' '}
+          <br className=" sm:block hidden" />
+        </h1>
+        <div className="ss:flex hidden md:mr-4 mr-0">
+          <GetStarted />
+        </div>
+      </div>
+
+      <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full">
+        Payment Method.
+      </h1>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        With the right credit card, you can improve your financial life by
+        building credit, earning rewards and saving money. But with hundreds of
+        credit cards on the market.
+      </p>
+    </div>
+    <div className={`flex flex-1 ${styles.flexCenter} md:my-0 my-10 relative`}>
+      <img
+        src={robot}
+        alt="robot"
+        className="w-[100%] h-[100%] relative z-[5]"
+      />
+      <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+      <div className="absolute z-[1] w-[80%] h-[80%] rounded-full top-0 bottom-40 pink__gradient" />
+      <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 top-0 blue__gradient" />
+    </div>
+    <div className={`ss:hidden ${styles.flexCenter}`}>
+      <GetStarted />
+    </div>
+  </section>
+);
 
 export default Hero;
